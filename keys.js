a478ad6f671f221e1f389b8297ea1fd9b79cbeea
tiffanyrudd <you@example.com>
2018-10-25 15:41:48 -0700

    functions created...

--- keys.js
+++ keys.js
@@ -4,5 +4,5 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
-;
+
 
--- liri.js
+++ liri.js
@@ -9,31 +9,64 @@ var Spotify = require('node-spotify-api');
 
 // local modules
 
-const keys = required("./keys.js");
+const keys = require("./keys.js");
  
 const action = process.argv[2];
 
 const input = process.argv[3];
 
 // const spotify = new Spotify(keys.spotify);
+if (!action) {
+    return console.log("you must provide an action")
+}
+if (!input) {
+    return console.log("you must provide an input for " + action)
+}
 
-console.log("KEYS", keys.Spotify); 
+
+console.log("KEYS", keys.spotify); 
 
 switch(action){
     case'concert-this':
-        console.log("You want a concert for ", input);
+    concertThis(input)
         break;
+
     case'spotify-this-song':
-        console.log("you want a song for ", input);
+        spotifyThisSong(input)
         break; 
+
     case 'movie-this': 
         console.log("you want movie info on ", input);
         break; 
+
     case 'do-what-it-says': 
-        console.log("Do what is says", input);
+        console.log("Do what it says", input);
         break; 
+
     default: 
         console.log("No idea what you are asking for.... "); 
 }
 
+// put all my functions below for each of the four actions listed in the homework instructions... starting with msContentScript, songs, movies.. 
 
+
+
+function concertThis(artist){
+    console.log("You want a concert for", artist);
+    //return the results from API call to node-spotify-api and log it out to the console// similiar to Thursdays class activity of changing info on the movies ei: year, plot, actors #18 or #19//
+}
+
+
+
+function spotifyThisSong(song){
+    console.log("You wnat a song for", song);
+}
+
+function movieThis(movie){
+    console.log("you want movie info on", movie);
+}
+
+
+function doWhatItSays(it){
+    console.log("Do what it says", it);
+}
